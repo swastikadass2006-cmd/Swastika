2758e42e3810b2ba3ca5a0f1943b5f873235d301
swastikadass2006-cmd <swastikadass2006@gmail.com>
2026-09-25 10:44:05 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (54.62%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int climbStairs(int n) {
+            if(n<=0) return 0;
+            if(n==1) return 1;
+            if(n==2) return 2;
+            int prev2=1;
+            int prev1=2;
+            for(int i=3;i<=n;i++){
+                int current= prev1+prev2;
+                prev2= prev1;
+                prev1=current;
+            }
+            return prev1;
+    }
+}
